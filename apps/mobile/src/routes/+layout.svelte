a5ad9ca1d7b70a04aa4bcff3f34b537e7d06809f
George Czabania <1140167+stayradiated@users.noreply.github.com>
2023-08-20 12:17:19 +0100

    feat(mobile): add a mobile nav menu

--- apps/mobile/src/routes/+layout.svelte
+++ apps/mobile/src/routes/+layout.svelte
@@ -1,31 +1,30 @@
-<script>
+<script lang="ts">
   import './water.css'
+  import { afterNavigate } from '$app/navigation';
+
+  let menuOpen = false
+  const toggleMenuOpen = () => menuOpen = !menuOpen;
+
+  afterNavigate(() => menuOpen = false)
 </script>
 
 <header>
   <h1>Pomo</h1>
 
-  <nav>
-    <ul>
-      <li>
-        <a href="/add">Add</a>
-      </li>
-      <li>
-        <a href="/quick-add">Quick Add</a>
-      </li>
-      <li>
-        <a href="/log">Log</a>
-      </li>
-      <li>
-        <a href="/calendar/day">Calendar</a>
-      </li>
-      <li>
-        <a href="/label">Labels</a>
-      </li>
-      <li>
-        <a href="/sync">Sync</a>
-      </li>
-    </ul>
+  <nav class:menuOpen={menuOpen}>
+    <div class="menuContainer">
+      <a href="/add">Add</a>
+      <a href="/quick-add">Quick Add</a>
+      <a href="/log">Log</a>
+      <a href="/calendar/day">Calendar</a>
+      <a href="/label">Labels</a>
+      <a href="/sync">Sync</a>
+    </div>
+    <button
+      class="menuToggle"
+      aria-hidden="true"
+      on:click={toggleMenuOpen}
+    >Menu</button>
   </nav>
 </header>
 
@@ -34,6 +33,7 @@
 <style>
   :global(body) {
     max-width: none;
+    overscroll-behavior-y: contain;
   }
 
   header {
@@ -42,12 +42,55 @@
     align-items: center;
   }
 
-  nav ul {
+  nav {
+    max-width: 1200px;
     display: flex;
-    padding: 0;
+    align-items: center;
+    margin: auto;
   }
-  nav li {
-    list-style-type: none;
-    margin-right: 1rem;
+
+  .menuContainer > a {
+    margin-left: 20px;
+  }
+
+  .menuToggle {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    .menuContainer {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 100vh;
+      height: -webkit-fill-available;
+      
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-end;
+
+      margin: 0;
+      padding-left: 7vw;
+      padding-right: 7vw;
+
+      background: lightblue;
+      z-index: 10;
+
+      opacity: 0;
+      transform: translateY(-100%);
+      transition: transform 0.2s, opacity 0.2s;
+    }
+
+    nav.menuOpen > .menuContainer {
+      opacity: 1;
+      transform: translateY(0);
+    }
+
+    .menuToggle {
+      display: initial;
+      z-index: 11;
+    }
   }
 </style>
